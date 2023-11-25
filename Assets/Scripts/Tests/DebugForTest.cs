9ade8907e09847ce7e9c04d13de1ef67db6d3069
postive12 <postive12@gmail.com>
2023-11-25 17:00:19 +0900

    Changed

---
+++ Assets/Scripts/Tests/DebugForTest.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.SceneManagement;
+
+namespace Tests
+{
+    public class DebugForTest : MonoBehaviour
+    {
+        [SerializeField] private string _scene01;
+        [SerializeField] private string _scene02;
+        private void Update()
+        {
+            if (Keyboard.current.spaceKey.wasPressedThisFrame) {
+                Debug.Log("Space Key Pressed");
+                SceneManager.LoadScene(_scene02,LoadSceneMode.Additive);
+            }
+        }
+    }
+}

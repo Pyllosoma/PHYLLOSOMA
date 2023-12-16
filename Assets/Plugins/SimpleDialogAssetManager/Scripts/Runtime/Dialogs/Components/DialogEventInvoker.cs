1f1b3d17598e720c3921c0808dabd02446464a5c
postive12 <postive12@gmail.com>
2023-12-16 22:06:10 +0900

    Add Save System

---
+++ Assets/Plugins/SimpleDialogAssetManager/Scripts/Runtime/Dialogs/Components/DialogEventInvoker.cs
@@ -0,0 +1,18 @@
+﻿using DialogSystem.Attributes;
+using DialogSystem.Dialogs.Components.Managers;
+using DialogSystem.Runtime.Dialogs.Interfaces;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace DialogSystem.Dialogs.Components
+{
+    public class DialogEventInvoker : DialogTargetComponent ,IEventInvoker
+    {
+        [SerializeField] private UnityEvent<string> _onInvokeEvent;
+        public void Invoke(string eventName) {
+            _onInvokeEvent?.Invoke(eventName);
+        }
+
+        
+    }
+}
---
+++ Assets/Plugins/SimpleDialogAssetManager/Scripts/Runtime/Dialogs/DialogPlotGraph.cs
@@ -0,0 +1,49 @@
+﻿using DialogSystem.Dialogs.Components.Managers;
+using DialogSystem.Nodes;
+using DialogSystem.Structure;
+using UnityEngine;
+using XNode;
+
+namespace DialogSystem.Runtime.Dialogs
+{
+    [CreateAssetMenu(menuName = "DialogSystem/DialogPlotGraph", fileName = "DialogPlotGraph")]
+    public class DialogPlotGraph : NodeGraph
+    {
+        public bool IsStart => _isStart;
+        [HideInInspector] public bool IsPlotEnd = true;
+        //Need to remake start and end point
+        public DialogBaseNode CurrentNode { get; private set; } = null;
+        private bool _isStart = false;
+        public void PlayPlot() {
+            _isStart = true;
+            IsPlotEnd = false;
+        }
+        public void Next(IDialogManager target) {
+            if (IsPlotEnd) {
+                return;
+            }
+            if (_isStart) {
+                _isStart = false;
+                CurrentNode = (DialogBaseNode)nodes[0];
+                CurrentNode.Play(target);
+            } else {
+                if (CurrentNode.CanGetNext()) {
+                    var lastNode = CurrentNode;
+                    var nextNode = CurrentNode.GetNext();
+                    lastNode.ResetNode();
+                    nextNode.ResetNode();
+                    CurrentNode = nextNode;
+                    CurrentNode.Play(target);
+                }
+            }
+            //If can load next node, load next node
+            if (!CurrentNode.IsNextExist()) {
+                //Debug.LogWarning("Can't go next");
+                IsPlotEnd = true;
+                CurrentNode = null;
+                return;
+            }
+            
+        }
+    }
+}
